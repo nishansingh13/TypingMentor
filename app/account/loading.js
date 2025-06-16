04846503a7bdb1b68be0e9097958a495c5c489df
nishansingh13 <nishansingh2480@gmail.com>
2025-06-16 17:26:45 +0530

    added some proper loaders

---
+++ app/account/loading.js
@@ -0,0 +1,14 @@
+import { Loader2 } from 'lucide-react';
+import Navbar from '../component/Navbar';
+
+export default function Loading() {
+  return (
+    <>
+    <Navbar/>
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] bg-gray-900">
+      <Loader2 className="h-12 w-12 text-emerald-400 animate-spin mb-4" />
+      <p className="text-gray-300">Loading data...</p>
+    </div>
+    </>
+  );
+}
